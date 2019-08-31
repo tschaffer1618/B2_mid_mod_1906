36f99dc6ef93cf9c4ce9ce6653762560c978d57c
Tyler Schaffer <tpschaffer@gmail.com>
2019-08-31 09:47:58 -0600

    Complete functionality for author show page

--- app/models/author.rb
+++ app/models/author.rb
@@ -4,4 +4,8 @@ class Author < ApplicationRecord
   has_many :books, through: :book_authors
 
   validates_presence_of :name
+
+  def average_book_length
+    books.average(:number_of_pages)
+  end
 end
