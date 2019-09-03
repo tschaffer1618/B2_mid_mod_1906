fff01f154c299e0bba598a887428e85a6d09b3bb
Tyler Schaffer <tpschaffer@gmail.com>
2019-09-02 22:14:34 -0600

    Remove user story 2 text

--- spec/features/authors/show_spec.rb
+++ spec/features/authors/show_spec.rb
@@ -19,14 +19,6 @@ describe "Author Show Page" do
     @robert.books << @zombie
   end
 
-  # As a user,
-  # When I visit the Book Index Page,
-  #   Then I see each author's name is a link
-  # When I click the link
-  #   Then I am taken to an Author Show Page where I see the author with that id including the author's:
-  #   - name
-  #   - the title of each book they have written
-  #   - the average number of pages for all of their books
   it "displays pertinent info about the author" do
     visit "/authors/#{@ernest.id}"
 
